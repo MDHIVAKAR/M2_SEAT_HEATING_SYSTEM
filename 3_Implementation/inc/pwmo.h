9843838c83f0966edf92eeca40f7dce1ed0399bb
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 22:54:03 +0530

    Update pwmo.h

--- 3_Implementation/inc/pwmo.h
+++ 3_Implementation/inc/pwmo.h
@@ -1,14 +1,3 @@
-/**
- * @file pwmo.h
- * @author JAHNAVI JHA
- * @brief Display the output through oscillator
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #ifndef pwmo_h
 #define pwmo_h
 
