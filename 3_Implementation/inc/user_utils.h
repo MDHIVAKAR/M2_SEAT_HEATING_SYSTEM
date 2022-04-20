26e4a3976f63df98aaaccaf852696fa29d770a12
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 22:55:09 +0530

    Update user_utils.h

--- 3_Implementation/inc/user_utils.h
+++ 3_Implementation/inc/user_utils.h
@@ -4,12 +4,6 @@
 #include "led.h"
 #include <util/delay.h>
 
-/**
- * @brief Function to generate delay in micro seconds
- * 
- * @param[in] delay_time Delay in Micro seconds
- * 
- */
 void delay_ms(uint32_t delay_time);
 
 #endif /* _USER_UTILS_H_ */
