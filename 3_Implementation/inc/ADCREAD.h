5a0e1554a9f407e35406e4ac34509f4f34e56849
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 12:51:26 +0530

    Create ADCREAD.h

---
+++ 3_Implementation/inc/ADCREAD.h
@@ -0,0 +1,21 @@
+/**
+ * @file ADCREAD.h
+ * @author JAHNAVI JHA
+ * @brief activity 2
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+#ifndef _ADCREAD_H_
+#define _ADCREAD_H_
+
+int ADCREAD();
+uint16_t ReadADC(uint8_t ch);
+void InitADC();
+
+#endif
