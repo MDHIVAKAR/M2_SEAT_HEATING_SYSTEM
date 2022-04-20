f1ef40ca2a1221df83f8f9fc4cef883206f68acf
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 22:52:52 +0530

    Update ADCREAD.h

--- 3_Implementation/inc/ADCREAD.h
+++ 3_Implementation/inc/ADCREAD.h
@@ -1,16 +1,3 @@
-/**
- * @file ADCREAD.h
- * @author JAHNAVI JHA
- * @brief activity 2
- * @version 0.1
- * @date 2021-04-28
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
-
-
 #ifndef _ADCREAD_H_
 #define _ADCREAD_H_
 
