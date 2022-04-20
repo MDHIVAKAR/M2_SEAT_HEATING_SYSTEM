86862755b6cef6f5b529e9433b3101b98c7b96c6
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 12:52:12 +0530

    Create display.h

---
+++ 3_Implementation/inc/display.h
@@ -0,0 +1,18 @@
+/**
+ * @file display.h
+ * @author JAHNAVI JHA
+ * @brief Send Data serially using UART
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef display_h
+#define display_h
+
+
+void display(char  *data);
+
+#endif
