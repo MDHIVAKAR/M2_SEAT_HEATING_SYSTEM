a0dfd6a9e72ad520b1beb1ee30ffc994e5a429dc
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 22:56:03 +0530

    Update display.c

--- 3_Implementation/src/display.c
+++ 3_Implementation/src/display.c
@@ -1,14 +1,3 @@
-/**
- * @file display.c
- * @author JAHNAVI JHA
- * @brief Activity 4
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #include<avr/io.h>
 #include<string.h>
 #include "display.h"
