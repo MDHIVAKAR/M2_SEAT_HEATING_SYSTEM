03d320c0496b61c62e4b430c482cc6601789903e
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 22:56:48 +0530

    Update pwmo.c

--- 3_Implementation/src/pwmo.c
+++ 3_Implementation/src/pwmo.c
@@ -1,13 +1,3 @@
-/**
- * @file pwmo.c
- * @author JAHNAVI JHA
- * @brief activity 3
- * @version 0.1
- * @date 2021-04-29
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 #include<avr/io.h>
 #include<string.h>
 
