4fccb5c20066a802a7edfd6cec319edeb3af6b51
DHIVAKAR M <101094751+MDHIVAKAR@users.noreply.github.com>
2022-04-20 12:53:43 +0530

    Create pwmo.h

---
+++ 3_Implementation/inc/pwmo.h
@@ -0,0 +1,17 @@
+/**
+ * @file pwmo.h
+ * @author JAHNAVI JHA
+ * @brief Display the output through oscillator
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef pwmo_h
+#define pwmo_h
+
+void pwmo(void);
+
+#endif
